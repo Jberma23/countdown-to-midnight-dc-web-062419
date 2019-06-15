81e873d20118819d5282d7f362ee07de3d9d79c4
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-15 21:14:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ while num > 0
   puts "#{num} SECOND(S)!"
   num -= 1
 end
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
 end
 countdown(4)
 
@@ -16,6 +16,6 @@ def countdown_with_sleep(num)
     num -= 1
     sleep 1
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
   end
 countdown(4)
